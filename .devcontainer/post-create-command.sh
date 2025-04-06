c378fe6cf5011e56311328221969217853732273
Mark Standen <no-reply@rundog.dev>
2025-04-06 17:57:29 +0000

    Removed comments from script and replaced with stdout messages.  Aids in debugging container builds

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "Running post-create-command.sh script"
-
 echo "Restoring NuGet packages..."
 dotnet restore /workspaces/run-suggestion/RunSuggestion.sln --verbosity minimal
 
+echo ""
 echo "Install Linux CLI tools"
 sudo apt-get update && sudo apt-get install -y \
     fzf \
     silversearcher-ag \
     tree
 
+echo ""
 echo "Define cleantree function to produce dir trees for readme"
 CLEANTREE_FUNC='function cleantree() { pwd && tree -d -I "obj|bin|images|nodejs|build"; }'
 
@@ -23,23 +23,16 @@ if [ -f ~/.zshrc ]; then
     echo "$CLEANTREE_FUNC" >> ~/.zshrc
 fi
 
+echo ""
 echo "Setting up Terraform tools..."
 if [ -f /workspaces/run-suggestion/tools/terraform/install.sh ]; then
     chmod +x /workspaces/run-suggestion/tools/terraform/install.sh
     /workspaces/run-suggestion/tools/terraform/install.sh
 fi
 
+echo ""
 echo "Setting up coding standards and Git hooks..."
 if [ -f /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh ]; then
     chmod +x /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh
     /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh
 fi
-
-echo "Source the updated shell profile"
-if [[ "$SHELL" == *"zsh"* ]]; then
-    source ~/.zshrc
-elif [[ "$SHELL" == *"bash"* ]]; then
-    source ~/.bashrc
-fi
-
-echo "post-create-command.sh script complete"
