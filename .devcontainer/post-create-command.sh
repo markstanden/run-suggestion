42acd560db391af755bbe8759394ded392521c08
Mark Standen <no-reply@rundog.dev>
2025-04-06 20:37:33 +0100

    Cicd pipeline (#3)
    * Adds devcontainer setup for running development environment within a container
    
    * Adds coding standards repo as a submodule
    
    * Uses SSH link to repo over http
    
    * Updates devcontainer setup
    
    * Adds vscode build configs
    
    * Updates post-create-command with better linux utils, coding standards setup, and terraform tools setup
    
    * Adds submodule symlinks to the gitignore.  These should be committed to upstream repos
    
    * Removed comments from script and replaced with stdout messages.  Aids in debugging container builds
    
    * Adds root pipeline orchestrator
    
    * Fixes unused parameters in workflow orchestrator
    
    * Fixes build issue with different dotnet versions between the container and dependencies
    
    * Moves order of dotnet update/restore until after submodule installation
    
    * Moves order of dotnet update/restore until after submodule installation
    
    * Removes duplicate references from Test project .csproj files
    
    * Readded test dependencies, fixing build bug

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo ""
+echo "Install Linux CLI tools"
+sudo apt-get update && sudo apt-get install -y \
+    fzf \
+    silversearcher-ag \
+    tree
+
+echo ""
+echo "Define cleantree function to produce dir trees for readme"
+CLEANTREE_FUNC='function cleantree() { pwd && tree -d -I "obj|bin|images|nodejs|build"; }'
+
+if [ -f ~/.bashrc ]; then
+    echo "Adding cleantree function to .bashrc"
+    echo "$CLEANTREE_FUNC" >> ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ]; then
+    echo "Adding cleantree function to .zshrc"
+    echo "$CLEANTREE_FUNC" >> ~/.zshrc
+fi
+
+echo ""
+echo "Setting up Terraform tools..."
+if [ -f /workspaces/run-suggestion/tools/terraform/install.sh ]; then
+    chmod +x /workspaces/run-suggestion/tools/terraform/install.sh
+    /workspaces/run-suggestion/tools/terraform/install.sh
+fi
+
+echo ""
+echo "Setting up coding standards and Git hooks..."
+if [ -f /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh ]; then
+    chmod +x /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh
+    /workspaces/run-suggestion/.coding-standards/dotnet/setup.sh
+fi
+
+echo ""
+echo "Updating .NET workloads..."
+sudo dotnet workload update
+
+echo "Restoring NuGet packages..."
+dotnet restore /workspaces/run-suggestion/RunSuggestion.sln --verbosity minimal
