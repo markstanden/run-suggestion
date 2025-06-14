d370c5087ab1aab963ce581d9e9bfca23abc7f80
Mark Standen <no-reply@rundog.dev>
2025-06-14 17:10:40 +0100

    Adds further tests / updates Transformer

--- tests/RunSuggestion.Core.Tests/Transformers/CsvToRunHistoryTransformerTests.cs
+++ tests/RunSuggestion.Core.Tests/Transformers/CsvToRunHistoryTransformerTests.cs
@@ -1,4 +1,6 @@
 using RunSuggestion.Core.Models;
+using RunSuggestion.Core.Models.DataSources.TrainingPeaks;
+using RunSuggestion.Core.Tests.TestHelpers;
 using RunSuggestion.Core.Transformers;
 
 namespace RunSuggestion.Core.Tests.Transformers;
@@ -8,21 +10,111 @@ public class CsvToRunHistoryTransformerTests
     private readonly CsvToRunHistoryTransformer _sut = new();
 
     [Fact]
-    public void ConvertToRunHistory_SingleValidRow_ReturnsOneRunEvent()
+    public void ConvertToRunHistory_SingleValidRunRow_ReturnsOneRunEvent()
     {
         // Arrange
-        const string CsvHeader = "\"Title\",\"WorkoutType\",\"WorkoutDay\",\"DistanceInMeters\",\"TimeTotalInHours\",\"HeartRateAverage\",\"HeartRateMax\",\"Rpe\",\"Feeling\"";
-        const string CsvRow = "\"Running\",\"Run\",\"2024-12-31\",\"12345.6789\",\"0.5\",\"150\",\"200\",\"3\",\"5\"";
-        const string Csv = $"{CsvHeader}\n{CsvRow}";
+        string csv = new TrainingPeaksCsvBuilder()
+            .AddRunningRow()
+            .Build();
 
         // Act
-        IEnumerable<RunEvent> result = _sut.Transform(Csv);
+        IEnumerable<RunEvent> result = _sut.Transform(csv);
 
         // Assert
         RunEvent runEvent = result.ShouldHaveSingleItem();
-        runEvent.Date.ShouldBe(new DateTime(2024, 12, 31));
-        runEvent.Distance.ShouldBe(12346);
-        runEvent.Duration.ShouldBe(TimeSpan.FromMinutes(30));
-        runEvent.Effort.ShouldBe((byte)3);
     }
+
+    [Theory]
+    [InlineData(2024, 12, 31)]
+    [InlineData(2025, 1, 1)]
+    public void ConvertToRunHistory_WhenPassedCorrectDateFormat_ParsesDateCorrectly(int year, int month, int day)
+    {
+        // Arrange
+        DateTime expectedDate = new(year, month, day);
+        string dateString = expectedDate.ToString(TrainingPeaksActivity.WORKOUT_DAY_DATETIME_FORMAT);
+        string csv = new TrainingPeaksCsvBuilder()
+            .AddRunningRow(dateString)
+            .Build();
+
+        // Act
+        IEnumerable<RunEvent> result = _sut.Transform(csv);
+
+        // Assert
+        RunEvent runEvent = result.ShouldHaveSingleItem();
+        runEvent.Date.ShouldBe(expectedDate);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(1000)]
+    [InlineData(10000)]
+    [InlineData(100000)]
+    public void ConvertToRunHistory_WhenPassedValidDistance_ParsesDistanceCorrectly(int distance)
+    {
+        // Arrange
+        string csv = new TrainingPeaksCsvBuilder()
+            .AddRunningRow(distanceInMeters: distance)
+            .Build();
+
+        // Act
+        IEnumerable<RunEvent> result = _sut.Transform(csv);
+
+        // Assert
+        RunEvent runEvent = result.ShouldHaveSingleItem();
+        runEvent.Distance.ShouldBe(distance);
+    }
+
+    [Theory]
+    [InlineData("Running", "Cardio", "Yoga", "Breathwork", 1)]
+    [InlineData("Running", "Running", "Yoga", "Breathwork", 2)]
+    [InlineData("Running", "Running", "Running", "Breathwork", 3)]
+    [InlineData("Running", "Running", "Running", "Running", 4)]
+    [InlineData("Cardio", "Running", "Yoga", "Breathwork", 1)]
+    [InlineData("Cardio", "Yoga", "Running", "Breathwork", 1)]
+    [InlineData("Cardio", "Yoga", "Breathwork", "Running", 1)]
+    public void ConvertToRunHistory_WhenPassedMultipleValidRowsWithMixedActivityTypes_ReturnsOnlyRunEvents(
+        string activity1, string activity2, string activity3, string activity4, int expectedCount)
+    {
+        // Arrange
+        string csv = new TrainingPeaksCsvBuilder()
+            .AddRow(activity1)
+            .AddRow(activity2)
+            .AddRow(activity3)
+            .AddRow(activity4)
+            .Build();
+
+        // Act
+        IEnumerable<RunEvent> results = _sut.Transform(csv);
+
+        // Assert
+        results.Count().ShouldBe(expectedCount);
+    }
+
+    [Fact]
+    public void Transform_WithNoActivityDataWithinCSV_ReturnsAnEmptyCollection()
+    {
+        // Arrange
+        string csv = new TrainingPeaksCsvBuilder().Build();
+
+        // Act
+        IEnumerable<RunEvent> results = _sut.Transform(csv);
+
+        // Assert
+        results.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Transform_WithAnEmptyCSV_ReturnsAnEmptyCollection()
+    {
+        // Arrange
+        string csv = string.Empty;
+
+        // Act
+        IEnumerable<RunEvent> results = _sut.Transform(csv);
+
+        // Assert
+        results.ShouldBeEmpty();
+    }
+
 }
