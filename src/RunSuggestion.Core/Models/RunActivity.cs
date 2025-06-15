3e8a8c56eb7339d79da685b3db1a3e50d0a14279
Mark Standen <no-reply@rundog.dev>
2025-06-15 17:19:57 +0100

    Adds docblock comments to run activity base class

--- src/RunSuggestion.Core/Models/RunActivity.cs
+++ src/RunSuggestion.Core/Models/RunActivity.cs
@@ -1,10 +1,38 @@
 namespace RunSuggestion.Core.Models;
 
+/// <summary>
+/// Base class used to reduce repetition between internal run models
+/// </summary>
 public class RunActivity
 {
+    /// <summary>
+    /// The serial for the ID within the database
+    /// </summary>
     public int Id { get; init; }
+
+    /// <summary>
+    /// The date that the run took place
+    /// </summary>
     public DateTime Date { get; init; }
+
+    /// <summary>
+    /// The overall distance (in metres) of the run
+    /// </summary>
     public int Distance { get; init; }
+
+    /// <summary>
+    /// Essentially the rate of perceived exhertion (RPE)
+    /// How difficult the run feels on a scale of 1-10
+    /// </summary>
     public byte Effort { get; init; }
+
+    /// <summary>
+    /// How long the run took, as a C# TimeSpan.
+    /// TimeSpans allow the duration to be expressed easily in any appropriate time measurement
+    /// </summary>
+    /// <example>
+    /// // Display as "45 minutes"
+    /// $"{Duration.TotalMinutes:F0} minutes"
+    /// </example>
     public TimeSpan Duration { get; init; }
 }
