4e26af176df7c97cf5cd7dae10c953a522edca7b
Mark Standen <no-reply@rundog.dev>
2025-06-15 21:57:33 +0100

    Adds docblock

--- src/RunSuggestion.Core/Services/AuthenticationService.cs
+++ src/RunSuggestion.Core/Services/AuthenticationService.cs
@@ -4,6 +4,13 @@ namespace RunSuggestion.Core.Services;
 
 public class AuthenticationService : IAuthenticator
 {
+    /// <summary>
+    /// Authenticates a user and returns a userId if authenticated.
+    /// Returns null if user is not authenticated.
+    /// Currently a stub implementation for development, so always returns 1.
+    /// </summary>
+    /// <param name="token">bearer token provided by the user to authenticate</param>
+    /// <returns>UserId if authentication succeeds, null if it fails. Currently prototype always returns 1</returns>
     public int? Authenticate(string token)
     {
         // Validate any token for now, return userId 1
