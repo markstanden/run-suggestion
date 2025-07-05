f7ad7503096797de33d71acb27784773020473b8
Mark Standen <no-reply@rundog.dev>
2025-07-05 08:06:31 +0100

    Updates the user repository interface with required methods.
    Adds Async suffix to asynchronous methods

--- src/RunSuggestion.Core/Interfaces/IUserRepository.cs
+++ src/RunSuggestion.Core/Interfaces/IUserRepository.cs
@@ -1,15 +1,37 @@
 using RunSuggestion.Core.Models.Runs;
+using RunSuggestion.Core.Models.Users;
 
 namespace RunSuggestion.Core.Interfaces;
 
 public interface IUserRepository
 {
     /// <summary>
+    /// Creates a new user in the database
+    /// </summary>
+    /// <param name="entraId">The GUID provided by entra to uniquely identify this user</param>
+    /// <returns>The internal userId of the user.</returns>
+    Task<int> CreateUserAsync(string? entraId);
+
+    /// <summary>
+    /// Retrieves a UserData object by userId from the database
+    /// </summary>
+    /// <param name="userId">The internal Id used to uniquely identify this user</param>
+    /// <returns>The full user data associated with the user</returns>
+    Task<UserData?> GetUserDataByUserIdAsync(int userId);
+    
+    /// <summary>
+    /// Retrieves a UserData object by entraId from the database
+    /// </summary>
+    /// <param name="entraId">The external Id used to uniquely identify this user by azure entra</param>
+    /// <returns>The full user data associated with the user</returns>
+    Task<UserData?> GetUserDataByEntraIdAsync(string entraId);
+    
+    /// <summary>
     /// Adds run history items to a user's run history.
     /// Returns the number of affected rows.
     /// </summary>
-    /// <param name="userId">The userId to associate the run history events with</param>
+    /// <param name="userId">The internal userId to associate the run history events with</param>
     /// <param name="runEvents">The run events to add to the user's history.</param>
-    /// <returns></returns>
-    Task<int> AddRunHistory(int userId, IEnumerable<RunEvent> runEvents);
+    /// <returns>The number of added rows within the database</returns>
+    Task<int> AddRunHistoryAsync(int userId, IEnumerable<RunEvent> runEvents);
 }
