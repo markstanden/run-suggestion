619d2e9c45d280747fd922963abb9fce69d3a03c
Mark Standen <no-reply@rundog.dev>
2025-07-05 12:41:28 +0100

    Adds documentation to DI constructor, and class as a whole.

--- src/RunSuggestion.Core/Repositories/UserRepository.cs
+++ src/RunSuggestion.Core/Repositories/UserRepository.cs
@@ -7,16 +7,23 @@ using RunSuggestion.Core.Sql;
 
 namespace RunSuggestion.Core.Repositories;
 
+/// <summary>
+/// Repository class isolating Database interactions behind a single class.
+/// </summary>
 public class UserRepository : IUserRepository
 {
     // Holding the connection as an instance variable prevents closure of the database and
     // allows the database to be accessed for the life of the class.
     // It would be more efficient to open and close the connection on read and write
     private readonly SqliteConnection _connection;
-
-
-
-
+    
+    /// <summary>
+    /// Dependency Injection constructor method to create a connection to the database and initialise.
+    /// Important in our case as for the prototype we are using an in memory SqlLite database, which will not
+    /// retain state between application runs.
+    /// This requires us to create the table each run, and pre-seed with fake user data to allow application testing.
+    /// </summary>
+    /// <param name="connectionString">Dependency injected connection string allows for different databases to be used in testing</param>
     public UserRepository(string connectionString)
     {
         _connection = new SqliteConnection(connectionString);
