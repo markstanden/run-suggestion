46039ff5be7631462129f64df44dc95712f7b9f5
Mark Standen <no-reply@rundog.dev>
2025-07-05 15:05:04 +0100

    Refactors with passing tests to extract creation of fake RunEvents to helper methods

--- tests/RunSuggestion.Core.Tests/Repositories/UserRepositoryTests.cs
+++ tests/RunSuggestion.Core.Tests/Repositories/UserRepositoryTests.cs
@@ -6,7 +6,7 @@ namespace RunSuggestion.Core.Tests.Repositories;
 
 public class UserRepositoryTests
 {
-    #region Common Setup
+    #region Common Test Setup
     private readonly UserRepository _sut;
     private const string TestConnectionString = "Data Source=:memory:";
 
@@ -18,6 +18,32 @@ public class UserRepositoryTests
 
     #region Test Helper Methods
     private string CreateFakeEntraId() => Guid.NewGuid().ToString();
+
+    /// <summary>
+    /// Test helper to create a fake run event.
+    /// The pattern of providing default null values for parameters and then assigning a default value within the helper
+    /// Allows default values to be provided by functions, which are not allowed in method signature. 
+    /// </summary>
+    /// <param name="userId">The UserId to assign the RunEvent to, defaults to 0</param>
+    /// <param name="dateTime">The date the event took place, defaults to Now</param>
+    /// <param name="distanceMetres">the distance of the run in metres - defaults to 5km</param>
+    /// <param name="effort">The effort score for the run (1-10), defaults to 5</param>
+    /// <param name="duration">The duration of the event as a TimeSpan.  Defaults to 30mins</param>
+    /// <returns>RunEvent</returns>
+    private RunEvent CreateFakeRunEvent(
+        int? userId = null, 
+        DateTime? dateTime = null, 
+        int? distanceMetres = null,
+        byte? effort = null,
+        TimeSpan? duration = null) => new RunEvent
+    {
+        Id = userId ?? 0,
+        Date = dateTime ?? DateTime.Now,
+        Distance = distanceMetres ?? 5000,
+        Effort = effort ?? 5,
+        Duration = duration ?? TimeSpan.FromMinutes(30)
+    };
+
     #endregion
 
     #region CreateUserAsync Tests
@@ -64,7 +90,7 @@ public class UserRepositoryTests
     public async Task CreateUserAsync_WithADuplicateEntraId_ShouldThrowInvalidArgument(string entraId)
     {
         // Arrange
-        int userId1 = await _sut.CreateUserAsync(entraId);
+        await _sut.CreateUserAsync(entraId);
 
         // Act
         var duplicateEntraId = async () => await _sut.CreateUserAsync(entraId);
@@ -100,14 +126,7 @@ public class UserRepositoryTests
         // Arrange
         int expectedRowCount = 1;
         int userId = await _sut.CreateUserAsync(CreateFakeEntraId());
-        RunEvent runEvent = new()
-        {
-            Id = userId,
-            Date = DateTime.Today,
-            Distance = 5000,
-            Effort = 5,
-            Duration = TimeSpan.FromMinutes(30)
-        };
+        RunEvent runEvent = CreateFakeRunEvent();
 
         // Act
         int result = await _sut.AddRunHistoryAsync(userId, [runEvent]);
