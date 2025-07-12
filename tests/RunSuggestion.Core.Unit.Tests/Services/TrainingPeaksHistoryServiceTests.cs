98c1482b646b2189cc02c3ed9f07886679302657
Mark Standen <no-reply@rundog.dev>
2025-07-12 17:13:35 +0100

    Creates initial test for run history service

---
+++ tests/RunSuggestion.Core.Unit.Tests/Services/TrainingPeaksHistoryServiceTests.cs
@@ -0,0 +1,37 @@
+using RunSuggestion.Core.Interfaces;
+using RunSuggestion.Core.Services;
+using RunSuggestion.Core.Unit.Tests.TestHelpers;
+
+namespace RunSuggestion.Core.Unit.Tests.Services;
+
+public class TrainingPeaksHistoryServiceTests
+{
+    private readonly Mock<IRunHistoryTransformer> _mockTransformer = new();
+    private readonly TrainingPeaksHistoryService _sut;
+
+    public TrainingPeaksHistoryServiceTests()
+    {
+        _sut = new TrainingPeaksHistoryService(_mockTransformer.Object);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(100)]
+    public void AddRunHistory_WhenTransformerReturnsValidRunEvents_ReturnsExpectedCount(int expectedEventCount)
+    {
+        // Arrange
+        int anyUserId = 1;
+        string validCsv = new TrainingPeaksCsvBuilder().Build();
+        var expectedRunEvents = Fakes.CreateRunEvents(expectedEventCount);
+        _mockTransformer.Setup(x => x.Transform(It.Is<string>(s => s == validCsv)))
+            .Returns(expectedRunEvents);
+
+        // Act
+        int result = _sut.AddRunHistory(anyUserId, validCsv);
+
+        // Assert
+        result.ShouldBe(expectedEventCount);
+    }
+}
