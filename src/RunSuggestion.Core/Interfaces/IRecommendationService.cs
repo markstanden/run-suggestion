1459bb17904c36b23eb05e836181f6334b5e1322
Mark Standen <no-reply@rundog.dev>
2025-08-09 14:48:37 +0100

    Renames `RunSuggestion` to `RunRecommendation` and introduces `IRecommendationService` interface to use it.

---
+++ src/RunSuggestion.Core/Interfaces/IRecommendationService.cs
@@ -0,0 +1,13 @@
+using RunSuggestion.Core.Models.Runs;
+
+namespace RunSuggestion.Core.Interfaces;
+
+public interface IRecommendationService
+{
+    /// <summary>
+    /// Creates a run recommendation based on the users' preferences and the recent run history.
+    /// </summary>
+    /// <param name="entraId">The User's Unique identifier within the authentication provider</param>
+    /// <returns>A run recommendation based on the users' preferences and run history</returns>
+    Task<RunRecommendation> GetRecommendation(string entraId);
+}
