eef6388ce5fde9aff6317b909d17452b58e0a9ee
Mark Standen <no-reply@rundog.dev>
2025-08-24 19:32:24 +0100

    Updates documentation, extracts common logic to keep class DRY.

--- src/RunSuggestion.Web/Authentication/StaticWebApp.cs
+++ src/RunSuggestion.Web/Authentication/StaticWebApp.cs
@@ -14,21 +14,24 @@ public static class StaticWebApp
 
     /// <summary>
     /// Builds the StaticWebApp login path including the redirect query using the specified redirect URL.
+    /// If passed redirect is null or empty string the query is omitted
     /// </summary>
     /// <param name="redirect">The URL to be redirected to following a successful login.</param>
-    /// <returns>Returns the full login path including the redirect query parameter.</returns>
+    /// <returns>Returns the full login path including the redirect query parameter if provided.</returns>
     public static string LoginPath(string? redirect) =>
-        string.IsNullOrWhiteSpace(redirect)
-            ? LoginBasePath
-            : $"{LoginBasePath}/{LoginRedirectQuery}={Uri.EscapeDataString(redirect)}";
+        PathWithRedirectQuery(LoginBasePath, LoginRedirectQuery, redirect);
 
     /// <summary>
     /// Builds the StaticWebApp logout path including the redirect query using the specified redirect URL.
+    ///     /// If passed redirect is null or empty string the query is omitted
     /// </summary>
     /// <param name="redirect">The URL to be redirected to following a successful logout.</param>
-    /// <returns>Returns the full logout path including the redirect query parameter.</returns>
+    /// <returns>Returns the full logout path including the redirect query parameter if provided.</returns>
     public static string LogoutPath(string? redirect) =>
-        string.IsNullOrWhiteSpace(redirect)
-            ? LogoutBasePath
-            : $"{LogoutBasePath}/{LogoutRedirectQuery}={Uri.EscapeDataString(redirect)}";
+        PathWithRedirectQuery(LogoutBasePath, LogoutRedirectQuery, redirect);
+
+    private static string PathWithRedirectQuery(string basePath, string query, string? redirect)
+        => string.IsNullOrWhiteSpace(redirect)
+            ? basePath
+            : $"{basePath}/{query}={Uri.EscapeDataString(redirect)}";
 }
