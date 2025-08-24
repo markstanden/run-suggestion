7753e62af02301dc0a3bf605ac1b0151dc1183ad
Mark Standen <no-reply@rundog.dev>
2025-08-24 11:21:23 +0100

    Creates Route constants file, uses it to replace magic string in RedirectToLogin.razor

---
+++ src/RunSuggestion.Web/Constants/Routes.cs
@@ -0,0 +1,7 @@
+namespace RunSuggestion.Web.Constants;
+
+public static class Routes
+{
+    public const string Login = "authentication/login";
+    public const string Logout = "authentication/logout";
+}
