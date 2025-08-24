9d5ad119dd2e642b8934dce4b401b6de3c93949f
Mark Standen <no-reply@rundog.dev>
2025-08-24 19:19:05 +0100

    Adds special characters and additional test case

--- tests/RunSuggestion.TestHelpers/Any.cs
+++ tests/RunSuggestion.TestHelpers/Any.cs
@@ -8,7 +8,10 @@ public static class Any
     public const string SixCharString = "abcdef";
     public const string ShortAlphanumericString = "ABC12345";
     public const string LongAlphanumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    public const string LongAlphaWithSpecialCharsString = "$$-ABCDEFGHIJKLMNOPQRSTUVWXYZ-&&-1234567890-%%";
+    public const string ShortAlphaWithSpecialCharsString = "$A^B*C(D)&1<2>3?4%";
+
+    public const string LongAlphaWithSpecialCharsString =
+        "$-A^B*C(D)E&F*G£H!I<J>K|L,M.N?O;P:Q@'R~S#T[U]V{W}X-Y_Z-+1=2`3¬4%";
 
     public const int Integer = 1;
 }
