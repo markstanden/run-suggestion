7ef525c63d42a563ea119d1c0874cda4c54c7682
Mark Standen <no-reply@rundog.dev>
2025-08-25 21:45:20 +0100

    Moves post login/logout to `Routes.Home`

--- src/Web/Constants/Routes.cs
+++ src/Web/Constants/Routes.cs
@@ -2,6 +2,7 @@ namespace RunSuggestion.Web.Constants;
 
 public static class Routes
 {
+    internal const string Home = "";
     public const string Login = "authentication/login";
     public const string Logout = "authentication/logout";
 }
