9bd97b9cb71b6d9a9208091311b185c9efe9cf3a
Mark Standen <no-reply@rundog.dev>
2025-08-27 20:02:29 +0100

    Creates basic frontend with simple status, extracts text into constants classes

---
+++ src/Web/Constants/ButtonText.cs
@@ -0,0 +1,9 @@
+namespace RunSuggestion.Web.Constants;
+
+public static class ButtonText
+{
+    public const string Login = "Login";
+    public const string Logout = "Logout";
+    public const string UploadHistory = "Upload";
+    public const string GetRecommendation = "Recommendation";
+}
---
+++ src/Web/Constants/Status.cs
@@ -0,0 +1,7 @@
+namespace RunSuggestion.Web.Constants;
+
+public static class Status
+{
+    public const string Uploading = "Uploading run history...";
+    public const string GettingRecommendation = "Getting recommendation...";
+}
