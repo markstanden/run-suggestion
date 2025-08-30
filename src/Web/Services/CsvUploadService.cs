3991e25cf59012aba6fb446531c02ae4bfbfa1b5
Mark Standen <no-reply@rundog.dev>
2025-08-30 16:16:06 +0100

    Passes tests

--- src/Web/Services/CsvUploadService.cs
+++ src/Web/Services/CsvUploadService.cs
@@ -8,7 +8,7 @@ public class CsvUploadService(ILogger<CsvUploadService> logger, HttpClient httpC
     private readonly ILogger<CsvUploadService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     private readonly HttpClient _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
 
-    public Task<bool> Upload(string csvContent)
+    public async Task<bool> Upload(string csvContent)
     {
         if (string.IsNullOrWhiteSpace(csvContent))
         {
@@ -17,6 +17,9 @@ public class CsvUploadService(ILogger<CsvUploadService> logger, HttpClient httpC
 
         _logger.LogInformation(Logs.Upload.Start);
 
-        return Task.FromResult(true);
+        HttpRequestMessage request = new(HttpMethod.Post, csvContent);
+        HttpResponseMessage result = await _httpClient.SendAsync(request);
+
+        return result.IsSuccessStatusCode;
     }
 }
