6cefc8451c4f4017a36a3a2f74c6452f88a2ed08
Mark Standen <no-reply@rundog.dev>
2025-08-31 15:13:52 +0100

    Updates Tests with failing Bearer token check

--- tests/Core.Unit.Tests/Services/AuthenticationServiceTests.cs
+++ tests/Core.Unit.Tests/Services/AuthenticationServiceTests.cs
@@ -1,23 +1,42 @@
 using RunSuggestion.Core.Services;
 using RunSuggestion.Shared.Constants;
+using RunSuggestion.TestHelpers;
 using RunSuggestion.TestHelpers.Theory;
 
 namespace RunSuggestion.Core.Unit.Tests.Services;
 
+[TestSubject(typeof(AuthenticationService))]
 public class AuthenticationServiceTests
 {
     [Theory]
     [MemberData(nameof(TestData.NullOrWhitespace), MemberType = typeof(TestData))]
-    public void Authenticate_WithInvalidToken_ThrowsException(string invalidToken)
+    public void ValidateToken_WithInvalidToken_ThrowsException(string invalidToken)
     {
         // Arrange
-        AuthenticationService sut = new();
+        const string expectedMessage = Errors.Authentication.NullOrWhitespaceToken;
 
         // Act
-        Func<string?> withInvalidToken = () => sut.Authenticate(invalidToken);
+        Func<string?> withInvalidToken = () => AuthenticationService.ValidateToken(invalidToken);
+
+        // Assert 
+        ArgumentException ex = withInvalidToken.ShouldThrow<ArgumentException>();
+        ex.Message.ShouldContain(expectedMessage);
+    }
+
+    [Theory]
+    [InlineData(Any.FourCharString)]
+    [InlineData(Any.FiveCharString)]
+    [InlineData(Any.SixCharString)]
+    public void ValidateToken_WithInvalidBearerToken_ThrowsArgumentException(string invalidBearerToken)
+    {
+        // Arrange
+        const string expectedMessage = Errors.Authentication.InvalidToken;
+
+        // Act
+        Func<string?> withInvalidBearerToken = () => AuthenticationService.ValidateToken(invalidBearerToken);
 
         // Assert
-        ArgumentException ex = withInvalidToken.ShouldThrow<ArgumentException>();
-        ex.Message.ShouldContain(Errors.Authentication.NullOrWhitespaceToken);
+        ArgumentException ex = withInvalidBearerToken.ShouldThrow<ArgumentException>();
+        ex.Message.ShouldContain(expectedMessage);
     }
 }
