ae19eecdbc1f06ced4731b5ba70b4cb6394f86d4
Mark Standen <no-reply@rundog.dev>
2025-09-05 08:27:20 +0100

    Adds methods to create Fake ClientPrincipals, and to encode as Base64 encoded json

--- tests/TestHelpers/Creators/AuthFakes.cs
+++ tests/TestHelpers/Creators/AuthFakes.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Microsoft.IdentityModel.Tokens;
 using RunSuggestion.Shared.Constants;
+using RunSuggestion.Shared.Models.Auth;
 
 namespace RunSuggestion.TestHelpers.Creators;
 
@@ -26,5 +27,33 @@ public static class AuthFakes
             ["userRoles"] = userRoles ?? ["anonymous", "authenticated"]
         });
         return Base64UrlEncoder.Encode(token);
+    }
+
+    /// <summary>
+    /// Creates a fake client principal for use in unit tests.
+    /// Provides defaults values for all parameters, but with overridable values for testing.
+    /// </summary>
+    /// <param name="userId">Sets the userId in the Client principal, defaults to Any.LongAlphaNumericString</param>
+    /// <param name="identityProvider">Sets the identityProvider in the Client principal, defaults to Any.ShortAlphaNumericString</param>
+    /// <param name="userDetails">Sets the userDetails in the Client principal, defaults to Any.ShortAlphaWithSpecialCharsString</param>
+    /// <returns><see cref="SwaClientPrincipal"/>With all values set</returns>
+    public static SwaClientPrincipal CreateFakeClientPrincipal(
+        string? userId = null,
+        string? identityProvider = null,
+        string? userDetails = null) =>
+        new()
+        {
+            UserId = userId ?? Any.LongAlphanumericString,
+            IdentityProvider = identityProvider ?? Any.ShortAlphanumericString,
+            UserDetails = userDetails ?? Any.ShortAlphaWithSpecialCharsString
+        };
+
+    public static string CreateFakeBase64SwaClientPrincipal(
+        string? userId = null,
+        string? identityProvider = null,
+        string? userDetails = null)
+    {
+        string json = JsonSerializer.Serialize(CreateFakeClientPrincipal(userId, identityProvider, userDetails));
+        return Base64UrlEncoder.Encode(json);
     }
 }
