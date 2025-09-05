c72d7500994564af4eb2fd947b68b13ef94ff81d
Mark Standen <no-reply@rundog.dev>
2025-09-05 18:28:25 +0100

    Adds documentation to whitespace theory memberdata

--- tests/TestHelpers/Theory/TestData.cs
+++ tests/TestHelpers/Theory/TestData.cs
@@ -5,8 +5,21 @@ namespace RunSuggestion.TestHelpers.Theory;
 public static class TestData
 {
     /// <summary>
-    /// Theory data for null, empty, and whitespace strings
+    /// Theory data for null, empty, and whitespace strings.
+    /// Used to test null, empty, and whitespace input validation.
     /// </summary>
+    /// <example>
+    /// <code>
+    /// [Theory]
+    /// [MemberData(nameof(TestData.NullOrWhitespace), MemberType = typeof(TestData))]
+    /// public void Method_WithEmptyInputString_DoesSomething(string whitespaceString)
+    /// {
+    ///     // Arrange
+    ///     // Act
+    ///     // Assert
+    /// }
+    /// </code> 
+    /// </example>
     public static readonly TheoryData<string> NullOrWhitespace = new()
     {
         null!,
