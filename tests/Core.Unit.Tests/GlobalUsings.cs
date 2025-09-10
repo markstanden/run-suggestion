6c9a2e269f54c411c790622ab355884abebb79ff
Mark Standen <no-reply@rundog.dev>
2025-09-10 10:36:18 +0100

    Adds Any and Whitespace imports to GlobalUsings.cs

--- tests/Core.Unit.Tests/GlobalUsings.cs
+++ tests/Core.Unit.Tests/GlobalUsings.cs
@@ -2,3 +2,7 @@ global using JetBrains.Annotations;
 global using Moq;
 global using Shouldly;
 global using Xunit;
+
+// Test helpers likely to be used in all tests
+global using RunSuggestion.TestHelpers;
+global using RunSuggestion.TestHelpers.Theory;
