9c286286bf42f956f3c5d8438d08166a2339dca9
Mark Standen <no-reply@rundog.dev>
2025-09-12 15:37:42 +0100

    Creates constants to describe the effort levels verbally

--- src/Shared/Constants/Runs.cs
+++ src/Shared/Constants/Runs.cs
@@ -4,8 +4,16 @@ public static class Runs
 {
     public const int RunDistanceBaseMetres = 1000;
     public const int RunDistanceBaseDurationMinutes = 15;
-    public const byte RunEffortBase = 1;
 
     public static readonly TimeSpan RunDistanceBaseDurationTimeSpan
         = TimeSpan.FromMinutes(RunDistanceBaseDurationMinutes);
+
+    public static class EffortLevel
+    {
+        public const byte Recovery = 1;
+        public const byte Easy = 2;
+        public const byte Medium = 3;
+        public const byte Strong = 4;
+        public const byte Hard = 5;
+    }
 }
